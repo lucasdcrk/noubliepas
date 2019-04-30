5cc0f8aeded89082a9e11c0ac6144411925e58ab
Lucas Decrock <lucasdecrock@outlook.com>
2019-04-30 09:22:47 +0200

    Wip

--- resources/views/reviser/afficher_carte.blade.php
+++ resources/views/reviser/afficher_carte.blade.php
@@ -5,13 +5,24 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">Carte #{{ $carte->id }} ({{ $carte->matiere->nom }})</div>
                     <div class="card-body">
-                        {{ $carte }}
-                        <a class="btn btn-success" href="">J'ai réusssi</a>
-                        <a class="btn btn-danger" href="">J'ai éprouvé des difficultés</a>
+                        <h6>Question (en {{ $carte->matiere->nom }}) :</h6>
+                        <h3>{{ $carte->recto }} ?</h3>
+                        <hr>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            Afficher la réponse
+                        </button>
+                        <div class="collapse mt-3" id="collapseExample">
+                            <div class="card card-body mb-3">
+                                <h6>Réponse :</h6>
+                                <h3>{{ $carte->verso }}</h3>
+                            </div>
+                            <a class="btn btn-success" href="">Je le savais</a>
+                            <a class="btn btn-danger" href="">J'ai oublié</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/reviser/choix_matiere.blade.php
+++ resources/views/reviser/choix_matiere.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">Choisissez une matière à réviser</div>
                     <div class="card-body">
